e84cd47abfff2285f092e7fa77729669832de87a
Yescafe <qyc027@gmail.com>
2018-09-23 07:12:00 +0800

    Add oj2057/demo.cpp

---
+++ oj2057/demo.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <string>
+using std::string;
+
+string plus(string &, string &);
+
+int main(void)
+{
+    using namespace std;
+    string num1, num2;
+
+    return 0;
+}

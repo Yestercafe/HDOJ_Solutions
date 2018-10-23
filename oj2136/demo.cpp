1edfe4df6e907fd96249101c827f7918edf88682
Yescafe <qyc027@gmail.com>
2018-10-23 21:34:10 +0800

    Modify oj2136 (mikansei*2)

--- oj2136/demo.cpp
+++ oj2136/demo.cpp
@@ -1,50 +1,42 @@
 //此题要求已经达成。但是没有办法优化时间。
-//
+//第二次修改，依然超时
 
 #include <iostream>
-#include <map>
-#include <utility>
 
 int main(void)
 {
     using namespace std;
     //ios::sync_with_stdio(false);
-
     int primeList[1000000];
-    primeList[1] = 1;
+    int primeCount[1000000];
+
+    primeList[1] = 0;
     for (int i = 2; i < 1000000; ++i) {
-        primeList[i] = 1;
+        primeList[i] = 0;
     }
 
-    for (int i = 2; i < 500000; ++i) {
-        if (primeList[i] == 1) {
+    for (int i = 2, c = 1; i < 1000000; ++i) {
+        if (primeList[i] == 0) {
             for (int j = 2; i * j < 1000000; ++j) {
                 primeList[i*j] = i;
             }
         }
-    }
-
-    /*map<int, int> primeCount;
-    for (int i = 2, pos = 1; i < 1000000; i++) {
-        if (primeList[i] == 1) {
-            primeCount.insert(make_pair(i, pos));
-            ++pos;
+        if (primeList[i] == 0) {
+            primeCount[i] = c;
+            primeList[i] = primeCount[i];
+            ++c;
+        } else {
+            primeList[i] = primeCount[primeList[i]];
         }
-    }*/
+    }
 
     int n;
     while (cin >> n) {
         if (n == 1) {
             cout << 0 << endl;
             continue;
-        }
-        for (int c = n; c > 1; --c) {
-            if (n / c * c == n) {
-                if (primeList[c] == true) {
-                    //cout << primeCount.find(c)->second << endl;
-                    break;
-                }
-            }
+        } else {
+            cout << primeList[n] << endl;
         }
     }
 

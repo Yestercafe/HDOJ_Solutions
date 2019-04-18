13c25daca51379cac883fb17a9b4941d6a461e5e
yescafe <qyc027@gmail.com>
2019-04-18 20:45:22 +0800

    Add oj2060

---
+++ oj2060/demo.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    while (cin >> t) {
+        int n, a, b;
+        while (t--) {
+            cin >> n >> a >> b;
+            if (n >= 6) {
+                a += 27 + 8 * (n - 6);
+            } else {
+                for (int i = 0; i < n; ++i) {
+                    a += 7 - i;
+                }
+            }
+
+            if (a >= b) {
+                cout << "Yes\n";
+            } else {
+                cout << "No\n";
+            }
+        }
+    }
+
+    return 0;
+}

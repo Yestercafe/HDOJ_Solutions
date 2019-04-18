9984202078dffea9db976e75f689fad122b76bc1
yescafe <qyc027@gmail.com>
2019-04-18 14:44:58 +0800

    Add oj1977

---
+++ oj1977/demo.cc
@@ -0,0 +1,25 @@
+// 提交的时候挺怂的，怕uint64_t都会炸，结果a了……
+#include <iostream>
+#include <cstdint>
+
+uint64_t powp3(const uint64_t &n) {
+    return n * n * n;
+}
+
+int main()
+{
+    using namespace std;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    while (cin >> t) {
+        while (t--) {
+            uint64_t a;
+            cin >> a;
+            cout << powp3(a) << ' ' << powp3(a + 1) << '\n';
+        }
+    }
+
+    return 0;
+}

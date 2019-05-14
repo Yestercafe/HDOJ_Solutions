e446af088e752ee290231b67f65e3ff09b0347e6
Yescafe <qyc027@gmail.com>
2019-05-14 10:42:48 +0800

    Add oj2212

---
+++ oj2212/demo.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // 越简单的题目越要优雅
+    cout << 1     << '\n'
+         << 2     << '\n'
+         << 145   << '\n'
+         << 40585 << '\n';
+
+    return 0;
+}
